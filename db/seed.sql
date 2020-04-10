937ed8ffa4851050e47e7b8ed2d1484c2093e9ee
Jacob Hall <halljaco@yahoo.com>
2020-04-10 15:49:07 -0600

    4 'o' clock commit

--- db/seed.sql
+++ db/seed.sql
@@ -1,6 +1,14 @@
--- CREATE TABLE  products (
---    id SERIAL PRIMARY KEY,
---    name VARCHAR(200),
---    price MONEY,
---    imgurl VARCHAR(2000)
--- )
+CREATE TABLE  products (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(200),
+  price NUMERIC,
+  imgurl VARCHAR(2000)
+)
+
+-- INSERT INTO products
+-- (name, price, imgurl)
+-- VALUES
+-- ('Shoes', 800.56, 'https://www.dhresource.com/0x0/f2/albu/g5/M00/1C/0F/rBVaI1kT3YiAEG6rAAXPswyptCg876.jpg'),
+-- ('Pants', 56.45, 'https://cdn.shopify.com/s/files/1/0454/6253/products/Carson_Denim_0001_D114R-CARSON-29627.jpg?v=1585687022'),
+-- ('Jacket', 239.00, 'https://assets.overland.com/is/image/overlandsheepskin/23701-brc2-av903549?$style%2Dlg%2D2x$'),
+-- ('hat', 110.75, 'https://cdn.shopify.com/s/files/1/2666/6234/products/designer-hats-unisex-women-and-men-baseball_grande_73ca74d7-fb7e-41d1-a791-ef977d56f40d_1024x1024.jpg?v=1572412687')
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -2,28 +2,64 @@ import React, { Component } from 'react';
 
 
 class Form extends Component {
-  constructor(props) {
-    super(props)
+   constructor(props) {
+      super(props)
 
-    this.state = {
-       name: "",
-       price: 0,
-       imgurl: ""
+      this.state = {
+         imgurl: "",
+         name: "",
+         price: 0
 
-    }
-    // Bind statements
-  }
+      }
+      // Bind statements
+   }
+
+   // handleChangeImgurl = () => {
+   //    this.setState({
+   //       imgurl: event.target.value
+   //    })
+   // }
+
+   // handleChangeProduct = () => {
+   //    this.setState({
+   //       name: event.target.value
+   //    })
+   // }
+
+   // handleChangePrice = () => {
+   //    this.setState({
+   //       price: event.target.value
+   //    })
+   // }
 
 
-  render() {
+
+   // handleChangeCancel = () => {
+   //    this.setState({
+         
+   //    })
+   // }
+
+   // handleChangeAdd = () => {
+   //    this.setState({
+
+   //    })
+   // }
+
+   render() {
 
 
-    return (
-      <div>
-         Form.js
-      </div>
-    )
-  }
+      return (
+         <div>
+            <img src="https://image.shutterstock.com/image-vector/default-ui-image-placeholder-wireframes-260nw-1037719192.jpg" alt="product image" />
+            <input placeholder="Insert Image url" />
+            <input placeholder="Insert Product Name" />
+            <input placeholder="Insert Price" />
+            <button onClick={this.handleChangeCancel}>Cancel</button>
+            <button onClick={this.handleChangeAdd}>Add to Inventory</button>
+         </div>
+      )
+   }
 }
 
 export default Form;
